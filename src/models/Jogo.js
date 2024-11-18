d8c2a0b277c0924dc5355228d65dedea4c916631
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2024-11-18 10:18:14 -0300

    feat: add o model do jogo

---
+++ src/models/Jogo.js
@@ -0,0 +1,40 @@
+class Jogo {
+    constructor(jogadores) {
+        this.id = Date.now() + Math.random();
+        this.jogadores = jogadores;
+        this.mapa = new Mapa();
+        this.jogadorAtual = 0;
+        this.rodada = 0;
+    }
+
+    iniciarJogo() {
+        // Configurações iniciais
+        this.rodada = 1;
+    }
+
+    proximoTurno() {
+        const jogadorAtual = this.jogadores[this.jogadorAtual];
+        const valorDado = jogadorAtual.rolarDado();
+        jogadorAtual.mover(valorDado);
+
+        const casaAtual = this.mapa.obterCasa(jogadorAtual.posicaoAtual);
+        
+        if (casaAtual.tipo === 'Cidade' && casaAtual.cidade.proprietario === null) {
+            // Lógica de compra de cidade
+            jogadorAtual.comprarCidade(casaAtual.cidade);
+        }
+
+        this.jogadorAtual = (this.jogadorAtual + 1) % this.jogadores.length;
+        
+        if (this.jogadorAtual === 0) {
+            this.rodada++;
+        }
+    }
+
+    verificarFimDeJogo() {
+        const jogadoresAtivos = this.jogadores.filter(j => !j.falido);
+        return jogadoresAtivos.length === 1 ? jogadoresAtivos[0] : null;
+    }
+}
+
+module.exports = { Jogador, Cidade, Mapa, Jogo };
